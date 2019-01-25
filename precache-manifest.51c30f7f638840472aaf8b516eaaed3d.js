0d9e3802c46ce4aa9eef19ff9d8630c90d1209a6
kushalkhadka7 <crkushal7@gmail.com>
2019-01-25 12:41:15 +0545

    Updates

---
+++ precache-manifest.51c30f7f638840472aaf8b516eaaed3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81db1de6bf475a96ff8",
+    "url": "/hackernews/static/css/main.9b3c665a.chunk.css"
+  },
+  {
+    "revision": "f81db1de6bf475a96ff8",
+    "url": "/hackernews/static/js/main.f81db1de.chunk.js"
+  },
+  {
+    "revision": "cf30c93f9b1e0cd4f96b",
+    "url": "/hackernews/static/js/1.cf30c93f.chunk.js"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "4bb3860da3aeaa84561e1836a78c77e9",
+    "url": "/hackernews/index.html"
+  }
+];
